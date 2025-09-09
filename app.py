9ab8f0bf7272c0586e3903cea4a8a35feb851089
Tobias Kessel <kesseltobias0@gmail.com>
2025-09-09 22:01:08 +0200

    Update server port to 5000 in app.py

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def display_checklist():
     return render_template("displayChecklist.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=448, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
